adb5d3566ba53104c9b52287bc53a1379c408e31
EvaM3 <tora.buke@gmail.com>
2022-01-07 01:23:50 +0100

    All tasks unit tests done, code changed

--- MyFocusTests/TaskTests.swift
+++ MyFocusTests/TaskTests.swift
@@ -12,6 +12,7 @@ class TaskTests: XCTestCase {
     
     let description1 = "Buy a few books"
     let description2 = "Buy some flowers"
+    let description3 = "Learn Russian today"
     
     func test_init() {
         let sut = makeSut()
@@ -20,35 +21,44 @@ class TaskTests: XCTestCase {
     }
     
     func test_completeTask() {
+        
         // ARRANGE:
-        var sut = Task(description: "Buy some flowers", completed: false, creationDate: Date(), achievedDate: Date())
-        XCTAssert(sut.description == "Buy some flowers" )
+        var sut = makeSut()
+        XCTAssert(sut.description == description3 )
         XCTAssertFalse(sut.completed, "Expected false, got\(sut.completed)")
+        
         // ACT:
         sut.completeTask()
+        
         // ASSERT:
         XCTAssertTrue(sut.completed)
         XCTAssertTrue((sut.achievedDate != nil))
     }
     
-    func test_completeTaskFalseAndNil() {
+    func test_completeTask_WhenFalse_ThenNotNil() {
+        
         // ARRANGE:
-        var sut = Task(description: "Buy some flowers", completed: false, creationDate: Date(), achievedDate: Date())
-        XCTAssert(sut.description == "Buy some flowers" )
+        var sut = makeSut()
+        XCTAssert(sut.description == description3)
         XCTAssertFalse(sut.completed, "Expected false, got\(sut.completed)")
+        
         // ACT:
         sut.completeTask()
+        
         // ASSERT:
         XCTAssertTrue(sut.completed)
-        XCTAssertNil(sut.achievedDate)
+        XCTAssertNotNil((sut.achievedDate))
     }
     
-    func test_completeTaskTrueAndFilled() {
+    func test_completeTask_ThenTrueAndFilled() {
+        
         // ARRANGE:
-        var sut = Task(description: "Buy some flowers", completed: false, creationDate: Date(), achievedDate: Date())
-        XCTAssert(sut.description == "Buy some flowers" )
+        var sut = makeSut()
+        XCTAssert(sut.description == description3 )
+        
         // ACT:
         sut.completeTask()
+        
         // ASSERT:
         XCTAssertTrue(sut.completed)
         XCTAssertTrue((sut.achievedDate != nil))
@@ -56,62 +66,77 @@ class TaskTests: XCTestCase {
     
 
     func test_undoCompletedTask() {
+        
         // ARRANGE:
         var sut = Task(description: "Learn Russian today", completed: true, creationDate: Date(), achievedDate: Date())
-        XCTAssert(sut.description == "Learn Russian today")
+        XCTAssert(sut.description == description3)
         XCTAssertTrue(sut.completed, "Expected true, got \(sut.completed)")
+        
         // ACT:
         sut.unDoCompleteTask()
+        
         // ASSERT:
         XCTAssertFalse(sut.completed)
         XCTAssertNil(sut.achievedDate)
     }
     
     
-    func test_undoCompleteTaskTrueandNil() {
+    func test_WhenUndoCompleteTask_ThenTrueandNil() {
+        
         // ARRANGE:
         var sut = Task(description: "Learn Russian today", completed: true, creationDate: Date(), achievedDate: Date())
-        XCTAssert(sut.description == "Learn Russian today")
+        XCTAssert(sut.description == description3)
         XCTAssertTrue(sut.completed, "Expected true, got \(sut.completed)")
+        
         // ACT:
         sut.unDoCompleteTask()
+        
         // ASSERT:
-        XCTAssertTrue(sut.completed)
+        XCTAssertFalse(sut.completed)
         XCTAssertNil(sut.achievedDate)
     }
     
-    func test_undoCompleteTaskFalseAndFilled() {
+    func test_undoCompleteTask_ThenFalseAndFilled() {
+        
         // ARRANGE:
-        var sut = Task(description: "Learn Russian today", completed: false, creationDate: Date(), achievedDate: Date())
-        XCTAssert(sut.description == "Learn Russian today")
+        var sut = makeSut()
+        XCTAssert(sut.description == description3)
         XCTAssertFalse(sut.completed, "Expected false, got \(sut.completed)")
+        
         // ACT:
         sut.unDoCompleteTask()
+        
         // ASSERT:
         XCTAssertFalse(sut.completed)
         XCTAssertNil((sut.achievedDate))
     }
     
-    func test_undoCompleteTaskFalseAndNil() {
+    func test_undoCompleteTask_ThenFalseAndNil() {
+        
         // ARRANGE:
-        var sut = Task(description: "Learn Russian today", completed: false, creationDate: Date(), achievedDate: Date())
-        XCTAssert(sut.description == "Learn Russian today")
+        var sut = makeSut()
+        XCTAssert(sut.description == description3)
         XCTAssertFalse(sut.completed, "Expected false, got \(sut.completed)")
+        
         // ACT:
         sut.unDoCompleteTask()
+        
         // ASSERT:
         XCTAssertFalse(sut.completed)
         XCTAssertNil(sut.achievedDate)
     }
      
     func test_updateTask() {
+        
         // ARRANGE:
-        var sut = Task(description: "Learn Russian today", completed: false, creationDate: Date(), achievedDate: Date())
-        XCTAssert(sut.description == "Learn Russian today")
+        var sut = makeSut()
+        XCTAssert(sut.description == description3)
+        
         // ACT:
         sut.unDoCompleteTask()
+        
         // ASSERT:
-        XCTAssert((sut.achievedDate != nil))
+        XCTAssertNotNil((sut.achievedDate != nil))
     }
     
     func makeSut() -> Task {
