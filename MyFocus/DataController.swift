071dfcee7d7f82f24655fa05f606c57f62cfcf83
EvaM3 <tora.buke@gmail.com>
2022-05-19 00:05:06 +0200

    Added DataController and more code

---
+++ MyFocus/DataController.swift
@@ -0,0 +1,73 @@
+//
+//  DataController.swift
+//  MyFocus
+//
+//  Created by Eva Madarasz on 18/05/2022.
+//
+
+import UIKit
+import CoreData
+
+
+class CoreDataManager {
+    
+    
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "TaskEntity")
+        
+        container.loadPersistentStores(completionHandler: { (NSPersistentStoreDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error)")
+            }
+        })
+        return container
+    }()
+    
+    
+    func saveData() {
+        
+        do {
+            try persistentContainer.viewContext.save()
+        } catch {
+            print("Error saving context \(error)")
+        }
+        
+    }
+    
+    func loadData(predicate: NSPredicate? = nil) -> [TaskEntity] {
+        let request : NSFetchRequest<TaskEntity> = TaskEntity.fetchRequest()
+        request.predicate = predicate
+        do {
+            return try persistentContainer.viewContext.fetch(request)
+        } catch {
+            print("Error loading data \(error)")
+            return []
+        }
+    }
+
+    func addItem(item: ListEntityUI) {
+        let newItem = TaskEntity(context: persistentContainer.viewContext)
+        newItem.name = item.title
+        newItem.creationDate = item.creationDate
+        newItem.achievedDate = item.achievedDate
+        saveData()
+    }
+    
+    func deleteData(at index: Int) {
+        let loadedData = loadData()
+        let selectedTask = loadedData[index]
+        persistentContainer.viewContext.delete(selectedTask)
+        self.saveData()
+    }
+    
+    func updateData(at index: Int, title: String) {
+        let loadedData = loadData()
+        let selectedTask = loadedData[index]
+        selectedTask.name = title
+        
+        self.saveData()
+        
+    }
+    
+}
--- MyFocus/ViewController.swift
+++ MyFocus/ViewController.swift
@@ -22,20 +22,21 @@ extension ListEntityUI {
     }
 }
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-            
-            DispatchQueue.main.async {
-                self.tableView.reloadData()
-            }
-            
         
-}
-
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+        
+        
+    }
+    let coreDataManager = CoreDataManager()
     var listEntityArray = [ListEntityUI]()
     @IBOutlet weak var tableView: UITableView!
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return listEntityArray.count
@@ -50,11 +51,84 @@ class ViewController: UIViewController {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-       // self.present(changeCancelAndEditTasks(indexPath: indexPath), animated: true)
+         self.present(changeCancelAndEditTasks(indexPath: indexPath), animated: true)
     }
     
     
     
     @IBAction func addButtonTapped(_ sender: Any) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add new task", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add task", style: .default) { (action) in
+            let newTask = ListEntityUI(title: textField.text ?? "", isCompleted: false, creationDate: Date() + 86400, achievedDate: nil)
+            self.coreDataManager.addItem(item: newTask)
+            self.loadData()
+        }
+        
+        alert.addAction(action)
+        alert.addTextField { (alertTextField) in alertTextField.placeholder = "New task here"
+            textField = alertTextField
+        }
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func updateTasks(indexPath: IndexPath) -> UIAlertController {
+        let item = listEntityArray[indexPath.row]
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Update task", message: "", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Update your task", style: .default, handler: { _ in
+            self.coreDataManager.updateData(at: indexPath.row , title: textField.text ?? "")
+            self.loadData()
+        }))
+        
+        
+        alert.addTextField { (alertTextField) in alertTextField.placeholder = "New task here"
+            textField = alertTextField
+        }
+        alert.textFields?.first?.text = item.title
+        return alert
+    }
+    
+    func changeCancelAndEditTasks(indexPath: IndexPath) -> UIAlertController {
+        let sheet = UIAlertController(title: "Change task", message: nil, preferredStyle: .alert)
+        sheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        sheet.addAction(UIAlertAction(title: "Edit", style: .default, handler: { _ in
+            self.present(self.updateTasks(indexPath: indexPath), animated: true)
+        } ))
+        sheet.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: { _ in
+            self.coreDataManager.deleteData(at: indexPath.row)
+            self.loadData()
+        }))
+        
+        return sheet
+    }
+    
+    
+    func map(item: TaskEntity) -> ListEntityUI {
+        
+        let newListEntity = ListEntityUI(mapListEntityUI: item)
+        return newListEntity
+    }
+    
+    func loadSortedData() {
+        let todaysSortDescriptor = NSSortDescriptor(key: "creationDate", ascending: true)
+        let sortDescriptors = [todaysSortDescriptor]
+        
+    }
+    
+    func loadData(pred: NSPredicate? = nil) {
+        let filteredFetchResult = coreDataManager.loadData(predicate: pred)
+        
+        listEntityArray = []
+        for item in filteredFetchResult {
+            let newMap = map(item: item)
+            listEntityArray.append(newMap)
+            
+        }
+        tableView.reloadData()
+    }
+    func saveData() {
+        coreDataManager.saveData()
+        self.loadData()
     }
 }
