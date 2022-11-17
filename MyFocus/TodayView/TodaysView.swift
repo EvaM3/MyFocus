58d2588234c59bd25c26e27da716543ddb257a29
EvaM3 <tora.buke@gmail.com>
2022-11-17 20:18:20 +0100

    Worked on TodaysViewController

--- MyFocus/TodayView/TodaysView.swift
+++ MyFocus/TodayView/TodaysView.swift
@@ -11,24 +11,25 @@ import UIKit
 
 
 class TodaysViewController: UIViewController {
-    
-    
+  
+
     @IBOutlet weak var tableView: UITableView!
+    
     
     
     var listModel: HistoryListModelProtocol = HistoryListModel(dataManager: CoreDataManager())
  
-    
+    var todaysItems: [ListElement] = [ListElement]()
     
     
     
     override func viewDidLoad() {
         
         
-           // tableView.dataSource = self
-           // tableView.delegate = self
-           // tableView.register(UINib(nibName: "GoalCell", bundle: nil), forCellReuseIdentifier: "goalCell")
-           // tableView.register(UINib(nibName: "TaskCell",bundle: nil), forCellReuseIdentifier: "taskCell")
+            tableView.dataSource = self
+            tableView.delegate = self
+            tableView.register(UINib(nibName: "GoalCell", bundle: nil), forCellReuseIdentifier: "goalCell")
+            tableView.register(UINib(nibName: "TaskCell",bundle: nil), forCellReuseIdentifier: "taskCell")
             super.viewDidLoad()
             DispatchQueue.main.async {
                 self.listModel.loadData()
@@ -40,9 +41,43 @@ class TodaysViewController: UIViewController {
     }
     
     // MARK: Tableview functions:
-//extension TodaysViewController: UITableViewDelegate, UITableViewDataSource {
+
+extension TodaysViewController: UITableViewDataSource, UITableViewDelegate {
     
-// }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todaysItems.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let todaysItem = todaysItems[indexPath.row]
+        
+        switch todaysItem.type {
+        
+        case .goal:
+            if let cell: GoalCell = tableView.dequeueReusableCell(withIdentifier: "goalCell", for: indexPath) as? GoalCell {
+                cell.configureCheckMarkedCell(item: todaysItem)
+                return cell
+            }
+            
+        case .task:
+            if let cell: TaskCell = tableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath) as? TaskCell {
+                cell.configureCheckMarkedCell(item: todaysItem)
+                return cell
+            }
+       
+        case .summary: break
+            
+        }
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        <#code#>
+    }
+    
+}
     
 
 
+}
