67b344d1f7876e692b74193b9168f5cb69036c77
EvaM3 <tora.buke@gmail.com>
2022-11-27 01:35:08 +0100

    Added some functions

--- MyFocus/TodayView/TodaysListModel.swift
+++ MyFocus/TodayView/TodaysListModel.swift
@@ -8,6 +8,20 @@
 import Foundation
 
 
+class TodaysListModel {
 
 var goalTitle = String()
 var taskTitle = String()
+var sections = [String]()
+var sectionRows =  [[ListElement]]()
+  
+    
+    func addGoal(goal: Goal) -> ListElement {
+        
+        let newGoal = ListElement(from: goal)
+    
+        return newGoal
+    }
+    
+    
+}
