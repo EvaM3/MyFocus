e744fc6059234b3a512ba0b4a0486ce7a99e5ea2
EvaM3 <tora.buke@gmail.com>
2022-12-30 01:39:54 +0100

    Worked on dataController unit tests

--- MyFocusTests/DataControllerTests.swift
+++ MyFocusTests/DataControllerTests.swift
@@ -10,7 +10,7 @@ import CoreData
 
 @testable import MyFocus
 
-var testManager = CoreDataManager()
+
 
 
 // let container = CoreDataContainer(name: "FocusData", inMemory: true)
@@ -33,7 +33,8 @@ var testManager = CoreDataManager()
 class testDataController: CoreDataManager {
     override init() {
         super.init()
-
+        
+ var testManager = CoreDataManager()
           
 let persistentStoreDescription = NSPersistentStoreDescription()
     persistentStoreDescription.type = NSInMemoryStoreType
@@ -53,7 +54,18 @@ let persistentStoreDescription = NSPersistentStoreDescription()
 
 class DataControllerTests: XCTestCase {
 
+    private var container: testDataController!
     
+    override func setUpWithError() throws {
+      let container = NSPersistentContainer(name: "FocusData")
+      container.loadPersistentStores { description, error in
+        XCTAssertNil(error)
+      }
+    }
+      
+    override func tearDownWithError() throws {
+      container = nil
+    }
 
     
     
